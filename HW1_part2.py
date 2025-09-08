1cab0faddf80ded49587c43eaa8a9b1736971390
Matt0604 <114870159+Matt0604@users.noreply.github.com>
2025-09-07 23:21:21 -0500

    Update HW1_part2.py
    This commit updates the code with a function for calculating the mean of a student's three exam scores.

--- HW1_part2.py
+++ HW1_part2.py
@@ -1,1 +1,16 @@
+# This code calculates the mean of a student's 3 exam scores and outputs the student's mean exam score
 
+# Define the exam_mean function for calculating the mean of 3 numbers
+def exam_mean(a, b, c):
+  mean = (a + b + c)/3
+  return mean
+
+# Define the inputs
+exam1 = 92
+exam2 = 64
+exam3 = 88
+
+# Call the function
+student_avg = exam_mean(exam1, exam2, exam3)
+
+print('Exam Average:' student_avg)

91f1531213570f0aa061fb409a49151941771b7a
Matt0604 <114870159+Matt0604@users.noreply.github.com>
2025-09-07 23:57:52 -0500

    Update HW1_part2.py
    This commit allows the user to input the 3 exam scores of a random student. The function is then called, then it ends with the inputs being repeated back to the user and the mean score being output.

--- HW1_part2.py
+++ HW1_part2.py
@@ -4,3 +4,15 @@
 def exam_mean(a, b, c):
   mean = (a + b + c)/3
   return mean
+
+# Take user input for each of the three exam scores
+score1 = float(input("Exam 1 Score: "))
+score2 = float(input("Exam 2 Score: "))
+score3 = float(input("Exam 3 Score: "))
+
+# Calculate the exam average for the semester
+semester_average = exam_mean(score1, score2, score3)
+
+# Print the inputs and output
+print(f"\nThe Student's three exam scores: {score1}, {score2}, {score3}")
+print(f"The Student's mean exam score is: {semester_average:.2f}")
